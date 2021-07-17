a7bd0b1cee6561e87d224b072d31290cba45a9f8
MelahGhislain <melahghislain17@gmail.com>
2021-07-17 23:14:59 +0100

    created the service details

--- capitalInsider/services/urls.py
+++ capitalInsider/services/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.servicePage, name="services")
+    path("", views.servicePage, name="services"),
+    path("detail/", views.servicePage, name="service-detail"),
 ]
