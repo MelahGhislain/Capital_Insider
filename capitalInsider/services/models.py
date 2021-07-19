23f89a1f3d2c44bf40ebb95df5b3e7068d837ed9
MelahGhislain <melahghislain17@gmail.com>
2021-07-19 11:51:36 +0100

    created the service model

--- capitalInsider/services/models.py
+++ capitalInsider/services/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Services(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField()
+    description = models.TextField(null=False)
+    cash_flow_statement = models.TextField()
+    balance_sheet_statement = models.TextField()
+    income_statement = models.TextField()
+    brainstorming = models.TextField()
+    plannification = models.TextField()
+    hard_work = models.TextField()
+    success = models.TextField()
+
+class Brochures(models.Model):
+    PDFfile = models.FileField()
+    DOCfile = models.FileField()
