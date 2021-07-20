e37da1923f80818eca4a7b042a35aa7b109d154d
MelahGhislain <melahghislain17@gmail.com>
2021-07-20 04:42:43 +0100

    setting up project app

--- capitalInsider/capitalInsider/urls.py
+++ capitalInsider/capitalInsider/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('services/', include('services.urls')),
+    path('projects/', include('projects.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
