0646c80315e6aeb12e3a6f402b858ee3b784d559
MelahGhislain <melahghislain17@gmail.com>
2021-07-21 23:08:52 +0100

    made services app dynamic

--- capitalInsider/projects/views.py
+++ capitalInsider/projects/views.py
@@ -5,14 +5,15 @@ from .models import Projects
 def projectPage(request):
     projects = Projects.objects.all().order_by("-year_completed")
     # will be use to filter  the projects to get mainImage, title, categoty
-    project_data = map(lambda x: {"category": x.category,
+    project_data = map(lambda x: {"id": x.id,
+                    "category": x.category,
                     "title": x.title,
                     "main_image": x.main_image
                      }, projects)
     project_categories = map(lambda x: {"category": x.category,}, projects)
 
     return render(request, "projects/index.html", {
-        "projects": project_data,
+        "project_data": project_data,
         "project_categories": project_categories,
         })
 
--- capitalInsider/services/models.py
+++ capitalInsider/services/models.py
@@ -3,15 +3,16 @@ from django.db import models
 # Create your models here.
 class Services(models.Model):
     title = models.CharField(max_length=50)
+    service_image = models.ImageField(null=False, blank=False)
     image = models.ImageField(blank=True)
     description = models.TextField(null=False)
-    cash_flow_statement = models.TextField()
-    balance_sheet_statement = models.TextField()
-    income_statement = models.TextField()
-    brainstorming = models.TextField()
-    plannification = models.TextField()
-    hard_work = models.TextField()
-    success = models.TextField()
+    cash_flow_statement = models.TextField(null=True, blank=True)
+    balance_sheet_statement = models.TextField(null=True, blank=True)
+    income_statement = models.TextField(null=True, blank=True)
+    brainstorming = models.TextField(null=True, blank=True)
+    plannification = models.TextField(null=True, blank=True)
+    hard_work = models.TextField(null=True, blank=True)
+    success = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
