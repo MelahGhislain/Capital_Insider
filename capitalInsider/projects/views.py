8fef0e5a5a671987334448c1e9c0dfeb0d57bec6
MelahGhislain <melahghislain17@gmail.com>
2021-07-21 14:13:33 +0100

    made project index.html dynamic

--- capitalInsider/projects/views.py
+++ capitalInsider/projects/views.py
@@ -3,10 +3,19 @@ from .models import Projects
 
 # Create your views here.
 def projectPage(request):
-    projects = Projects.objects.all()# .order_by("-year_completed")
+    projects = Projects.objects.all().order_by("-year_completed")
     # will be use to filter  the projects to get mainImage, title, categoty
-    # project_data = projects.
-    return render(request, "projects/index.html", {"projects": projects})
+    project_data = map(lambda x: {"category": x.category,
+                    "title": x.title,
+                    "main_image": x.main_image
+                     }, projects)
+    project_categories = map(lambda x: {"category": x.category,}, projects)
 
-def projectDetail(request):
+    return render(request, "projects/index.html", {
+        "projects": project_data,
+        "project_categories": project_categories,
+        })
+
+def projectDetail(request, id):
+    projects = Projects.objects.all()
     return render(request, 'projects/project_detail.html')
