db451f4a7154e2734b3f9605f93d55d68ba28142
MelahGhislain <melahghislain17@gmail.com>
2021-07-21 11:10:23 +0100

    updated urls

--- capitalInsider/projects/views.py
+++ capitalInsider/projects/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def projectPage(request):
     return render(request, "projects/index.html")
+
+def projectDetail(request):
+    return render(request, 'projects/project_detail.html')
