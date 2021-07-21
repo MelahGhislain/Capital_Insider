b17e9d755695dc4e900a395483f2a36319e0c797
MelahGhislain <melahghislain17@gmail.com>
2021-07-21 11:20:05 +0100

    create the project model

--- capitalInsider/projects/models.py
+++ capitalInsider/projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class projects(models.Model):
+    client = models.CharField(max_length=50, blank=True,null=True)
+    year_completed = models.DateTimeField(blank=True,null=True)
+    value = models.FloatField(blank=True,null=True)
+    adviser = models.CharField(max_length=150)
+    category = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=False,blank=False)
+    main_image = models.ImageField(blank=False, null=False)
+    image1 = models.ImageField(blank=True, null=True)
+    image2 = models.ImageField(blank=True, null=True)
