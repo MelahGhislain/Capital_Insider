37c9897781291ab65a934be7762bb0e5410daa58
MelahGhislain <melahghislain17@gmail.com>
2021-07-22 17:18:44 +0100

    created the about model

--- capitalInsider/about/models.py
+++ capitalInsider/about/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AboutUs(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+
+
+class  Expert(models.Model):
+    image = models.ImageField(null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    position = models.CharField(max_length=100, null=False, blank=False)
