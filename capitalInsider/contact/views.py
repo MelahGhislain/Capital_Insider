c9d3ecfe4c272b5c069282f6125b05c587d48395
MelahGhislain <melahghislain17@gmail.com>
2021-07-24 20:54:50 +0100

    setup the contact template

--- capitalInsider/contact/views.py
+++ capitalInsider/contact/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import FinancialAdvisor, ContactInfo, SocialNetwork
 # Create your views here.
 def contactPage(request):
-    return render(request, "contact/index.html")
+    advisors = FinancialAdvisor.objects.all()
+    contact = ContactInfo.objects.last()
+    social_networks = SocialNetwork.objects.all()
+    return render(request, "contact/index.html", {
+        "advisors": advisors,
+        "contact": contact,
+        "socials": social_networks,
+    })
