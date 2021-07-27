560a461813a2e0d7c1a2c0e4396831aba9fe68a0
MelahGhislain <melahghislain17@gmail.com>
2021-07-27 05:20:49 +0100

    updated the home view

--- capitalInsider/home/models.py
+++ capitalInsider/home/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 class Partner(models.Model):
     logo = models.ImageField()
+    link = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.logo.name
 
--- capitalInsider/home/views.py
+++ capitalInsider/home/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from about.models import AboutUs, Expert
+from services.models import Services
+from projects.models import Projects
+from .models import Partner
 
 # Create your views here.
 
 def homePage(request):
-    return render(request, "home/index.html")
+    services = Services.objects.all()
+    projects = Projects.objects.all()
+    experts = Expert.objects.all()
+    partners = Partner.objects.all()
+    context = {
+        "services": services,
+        "projects": projects,
+        "experts": experts,
+        "partners": partners,
+    }
+    return render(request, "home/index.html", context)
