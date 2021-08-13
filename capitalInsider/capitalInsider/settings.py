a0fe9aad2f2ef2bd0e6504cb191ba87225fd5f7d
MelahGhislain <melahghislain17@gmail.com>
2021-08-13 04:31:17 +0100

    updates

--- capitalInsider/capitalInsider/settings.py
+++ capitalInsider/capitalInsider/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'home.apps.HomeConfig',
     # 'crispy_forms'
-    'base'
+    'base',
     'home',
     'services',
     'projects',
