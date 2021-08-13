95c002c2df84661f58dd51a7bfeef97663d39843
MelahGhislain <melahghislain17@gmail.com>
2021-08-13 08:42:57 +0100

    created a footer view

--- capitalInsider/base/views.py
+++ capitalInsider/base/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from services.models import Services
+from contact.models import ContactInfo, SocialNetwork
 
 # Create your views here.
+def footer(request):
+    titles = Services.objects.get("title")
+    contact_info = ContactInfo.objects.all()
+    social_icons = SocialNetwork.objects.all()
+    context = {
+        "titles": titles,
+        "contact_info": contact_info,
+        "social_icons": social_icons
+    }
+    return render(request, 'footer.html', context)
