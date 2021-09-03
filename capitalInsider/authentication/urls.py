947ba64233a52f5bd72e026a1165d15b3215fecf
MelahGhislain <melahghislain17@gmail.com>
2021-09-03 09:44:49 +0100

    work on the registration view

--- capitalInsider/authentication/urls.py
+++ capitalInsider/authentication/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path("login/", views.login, name="about")
+    path("login/", views.loginPage, name="login"),
+    path("signup/", views.signUpPage, name="signup"),
+    path("forgot-password/", views.forgotPassword, name="forgot-password"),
 ]
--- capitalInsider/authentication/views.py
+++ capitalInsider/authentication/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .forms import CreateUserForm
 
 # Create your views here.
-def login(request):
+def loginPage(request):
     return render(request, 'authenticate/login.html')
+
+
+def signUpPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {"form": form}
+    return render(request, 'authenticate/signup.html', context)
+
+
+def forgotPassword(request):
+    return render(request, 'authenticate/forgot-password.html')
