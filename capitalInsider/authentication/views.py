dea3fa26dfdfd9470eaec362095536510ff8a1b8
MelahGhislain <melahghislain17@gmail.com>
2021-09-03 12:07:20 +0100

    created  the functionality for user registration and login

--- capitalInsider/authentication/views.py
+++ capitalInsider/authentication/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render,  redirect
 from .forms import CreateUserForm
 from  django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
     return render(request, 'authenticate/login.html')
 
 
@@ -13,7 +24,9 @@ def signUpPage(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data.get('username')
             form.save()
+            messages.success(request, f"Account created successfully for {username}")
             return redirect('login')
     context = {"form": form}
     return render(request, 'authenticate/signup.html', context)
