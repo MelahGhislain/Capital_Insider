697e2c8fa62dfffdb222054dcfdddaa553da805b
MelahGhislain <melahghislain17@gmail.com>
2021-09-03 07:37:28 +0100

    create an authentication app

--- capitalInsider/capitalInsider/urls.py
+++ capitalInsider/capitalInsider/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('auth.urls')),
     path('', include('home.urls')),
     path('services/', include('services.urls')),
     path('projects/', include('projects.urls')),
